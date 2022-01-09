51ce765da82ea81109dff1f0c3a23dfb50db354a
ytazz <tazaki@mech.kobe-u.ac.jp>
2022-01-09 14:55:15 +0900

    migrated from systembuilder

---
+++ include/sbblas.h
@@ -0,0 +1,180 @@
+#pragma once
+
+#include <sbtypes.h>
+
+#include <map>
+
+namespace Scenebuilder{;
+
+struct Vector{
+	int     n;
+	double* vh;
+	
+	void Delete  ();
+	void Allocate(int _n);
+	void Resize  (int _n);
+
+	double& operator()(int i){ return vh[i]; }
+
+	Vector SubVector(int ofst, int sz);
+
+	 Vector();
+	 Vector(int _n);
+	~Vector();
+};
+
+struct Matrix{
+	int     m, n, l;  //< rows, cols, leading-dim
+	double* vh;
+	
+	void Delete  ();
+	void Allocate(int _m, int _n);
+	void Resize  (int _m, int _n);
+
+	double& operator()(int i, int j){ return vh[l*j+i]; }
+
+	Matrix SubMatrix(int row, int col, int w, int h);
+
+	 Matrix();
+	 Matrix(int _m, int _n);
+	~Matrix();
+};
+
+struct SparseVector : std::map<int, Vector>{
+	int n;
+	int dim;
+	//std::vector< std::map<int, Vector>::iterator >  index;
+
+	void          Resize   (int _n, int _dim);
+	const Vector* Item     (int i) const;
+	Vector        SubVector(int i);
+	void          Clear    (int i);
+	void          Clear    ();
+};
+
+struct SparseMatrix{
+	int           m;
+	int           n;
+	int           dim;
+
+	struct Row : std::map<int, Matrix>{
+		//std::vector< std::map<int, Matrix>::iterator >  index;
+	};
+	vector<Row>     rows;   //< number of non-zeros of each row
+	
+	void           Resize   (int _m, int _n, int _dim);
+	const Matrix*  Item     (int i, int j) const;
+	Matrix         SubMatrix(int i, int j);
+	Matrix         SubMatrix(int i, int j) const;
+	SparseMatrix   RowMatrix(int i) const;
+	SparseMatrix   ColMatrix(int j) const;
+	void           Clear    ();
+	void           RowClear (int i);
+	void           ColClear (int j);
+
+	void  PrintSparsity(ostream& os);
+
+	SparseMatrix();
+};
+
+ostream& operator<<(ostream& os, Vector& v);
+ostream& operator<<(ostream& os, Matrix& m);
+
+void  mat_eye    (Matrix& m);
+void  mat_diag   (const Vector& v , Matrix& y);
+void  mat_eig    (const Matrix& m , Vector& wr, Vector& wi, Matrix& vl, Matrix& vr);
+void  mat_inv_gen(const Matrix& m , Matrix& y);
+void  mat_inv_pd (const Matrix& m , Matrix& y);
+
+void  vec_clear(Vector      &  y);
+void  vec_clear(Vector      && y);
+void  vec_clear(SparseVector&  y);
+void  mat_clear(Matrix      &  y);
+void  mat_clear(Matrix      && y);
+void  mat_clear(SparseMatrix&  y);
+void  vec_copy (const Vector      & v1, Vector      &  y);
+void  vec_copy (const Vector      & v1, Vector      && y);
+void  vec_copy (const SparseVector& v1, SparseVector&  y);
+void  mat_copy (const Matrix      & m1, Matrix      &  y);
+void  mat_copy (const Matrix      & m1, Matrix      && y);
+void  mat_copy (const SparseMatrix& m1, SparseMatrix&  y);
+
+template<size_t N, class D>
+void vec_copy(const PTM::TVectorBase<N, D>& v, Vector& y){
+	typedef typename D::element_type T;
+	const T* v0 = &v[0];
+	double*  v1 = y.vh;
+	for(int i = 0; i < N; i++)
+		*v1++ = (double)*v0++;
+}
+
+template<class T>
+void vec_copy(const PTM::VVector<T>& v, Vector& y){
+	const T* v0 = &v[0];
+	double*  v1 = y.vh;
+	for(int i = 0; i < y.n; i++)
+		*v1++ = (double)*v0++;
+}
+
+template<size_t H, size_t W, class D>
+void  mat_copy (const PTM::TMatrixBase<H, W, D>& m1, Matrix&& y){
+	typedef typename D::element_type T;
+	const T* col0 = &m1[0][0];
+	double*  col1 = y .vh;
+	for(int j = 0; j < W; j++, col0 += H, col1 += y.l){
+		const real_t* v0 = col0;
+		double*       v1 = col1;
+		for(int i = 0; i < H; i++){
+			*v1++ = (double)*v0++;
+		}
+	}
+}
+template<size_t H, size_t W, class D>
+void  mat_copy (const PTM::TMatrixBase<H, W, D>& m1, Matrix& y){
+	mat_copy(m1, std::move(y));
+}
+
+template<class T>
+void mat_copy(const PTM::VMatrixCol<T>& m, Matrix& y){
+	const T* col0 = &m[0][0];
+	double*  col1 = y.vh;
+	for(int j = 0; j < y.n; j++, col0 += y.m, col1 += y.l){
+		const T* v0 = col0;
+		double*  v1 = col1;
+		for(int i = 0; i < y.m; i++){
+			*v1++ = (double)*v0++;
+		}
+	}
+}
+
+double mat_abs       (const Matrix& m);
+void   vec_add       (const Vector&       v1, Vector&       y);
+void   vec_add       (const Vector&       v1, Vector&&      y);
+void   vec_add       (const SparseVector& v1, SparseVector& y);
+void   mat_add       (const Matrix&       m1, Matrix&       y);
+void   mat_add       (const Matrix&       m1, Matrix&&      y);
+void   mat_add       (const SparseMatrix& m1, SparseMatrix& y);
+double vec_dot       (const Vector&       v1, const Vector&       v2);
+void   mat_vec_mul   (const Matrix&       m1, const Vector&        v, Vector&       y, double alpha, double beta);
+void   mat_vec_mul   (const Matrix&       m1, const Vector&        v, Vector&&      y, double alpha, double beta);
+void   mat_vec_mul   (const SparseMatrix& m1, const SparseVector&  v, Vector&       y, double alpha, double beta);
+void   symmat_vec_mul(const Matrix&       m1, const Vector&        v, Vector&       y, double alpha, double beta);
+void   mattr_vec_mul (const Matrix&       m1, const Vector&        v, Vector&       y, double alpha, double beta);
+void   mattr_vec_mul (const Matrix&       m1, const Vector&        v, Vector&&      y, double alpha, double beta);
+void   mattr_vec_mul (const SparseMatrix& m1, const Vector&        v, SparseVector& y, double alpha, double beta);
+void   mat_mat_mul   (const Matrix&       m1, const Matrix&       m2, Matrix&       y, double alpha, double beta);
+void   mat_mat_mul   (const Matrix&       m1, const Matrix&       m2, Matrix&&      y, double alpha, double beta);
+//void   mat_mat_mul   (const SparseMatrix& m1, const Matrix&       m2, SparseMatrix& y, double alpha, double beta);
+void   mattr_mat_mul (const Matrix&       m1, const Matrix&       m2, Matrix&       y, double alpha, double beta);
+void   mattr_mat_mul (const Matrix&       m1, const Matrix&       m2, Matrix&&      y, double alpha, double beta);
+void   mattr_mat_mul (const Matrix&       m1, const SparseMatrix& m2, SparseMatrix& y, double alpha, double beta);
+void   mattr_mat_mul (const SparseMatrix& m1, const SparseMatrix& m2, SparseMatrix& y, double alpha, double beta);
+void   mat_mattr_mul (const Matrix&       m1, const Matrix&       m2, Matrix&       y, double alpha, double beta);
+void   symmat_mat_mul(const Matrix&       m1, const Matrix&       m2, Matrix&       y, double alpha, double beta);
+void   symmat_mat_mul(const Matrix&       m1, const Matrix&       m2, Matrix&&      y, double alpha, double beta);
+void   symmat_mat_mul(const Matrix&       m1, const SparseMatrix& m2, SparseMatrix& y, double alpha, double beta);
+void mattr_mat_mul_batch(const Matrix& m1, const Matrix& m2, Matrix& y, double alpha, double beta);
+
+void Linsolve(SparseMatrix& A, SparseVector& b, SparseVector& x, bool mindeg, real_t regularization);
+
+}
---
+++ src/sbblas.cpp
@@ -0,0 +1,681 @@
+#include <sbblas.h>
+
+#define USE_MKL
+//#define USE_CUBLAS
+
+#if defined USE_MKL
+# ifdef _WIN32
+#  include <mkl.h>
+//#  include <mkl_lapacke.h>
+//#  include <mkl_cblas.h>
+# else
+#  include <mkl.h>
+//#  include <lapacke.h>
+# endif
+#endif
+
+#if defined USE_CUBLAS
+# include <cuda_runtime.h>
+# include <cublas_v2.h>
+# include <cusolverDn.h>
+#endif
+
+#include <set>
+
+namespace Scenebuilder{
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+Vector::Vector(){
+	n      = 0;
+	vh     = 0;
+}
+
+Vector::Vector(int _n){
+	n      = 0;
+	vh     = 0;
+	Allocate(_n);
+}
+
+Vector::~Vector(){
+	Delete();
+}
+
+Vector Vector::SubVector(int ofst, int sz){
+	Vector y;
+	y.vh = vh + ofst;
+	y.n  = sz;
+	return y;
+}
+
+void Vector::Delete(){
+	//if(vh) delete[] vh;
+}
+
+void Vector::Allocate(int _n){
+	Delete();
+	vh = new double[_n];
+		
+	n = _n;
+}
+
+void Vector::Resize(int _n){
+	n = _n;
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+Matrix::Matrix(){
+	m  = 0;
+	n  = 0;
+	l  = 0;
+	vh = 0;
+}
+
+Matrix::Matrix(int _m, int _n){
+	m  = 0;
+	n  = 0;
+	l  = 0;
+	vh = 0;
+	Allocate(_m, _n);
+}
+
+Matrix::~Matrix(){
+	//if(vh) delete[] vh;
+}
+
+void Matrix::Delete(){
+	if(vh) delete[] vh;
+}
+
+Matrix Matrix::SubMatrix(int row, int col, int _m, int _n){
+	Matrix y;
+	y.vh = vh + row + l*col;
+	y.m = _m;
+	y.n = _n;
+	y.l =  l;
+	return y;
+}
+
+void Matrix::Allocate(int _m, int _n){
+	Delete();
+	vh = new double[_m*_n];
+	
+	m = _m;
+	n = _n;
+	l = _m;
+}
+
+void Matrix::Resize(int _m, int _n){
+	m = _m;
+	n = _n;
+}
+
+ostream& operator<<(ostream& os, Vector& v){
+	for(int i = 0; i < v.n; i++)
+		os << v(i) << " ";
+	return os;
+}
+
+ostream& operator<<(ostream& os, Matrix& m){
+	for(int i = 0; i < m.m; i++)for(int j = 0; j < m.n; j++)
+		os << m(i,j) << " ";
+	return os;
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+void SparseVector::Resize(int _n, int _dim){
+	n   = _n;
+	dim = _dim;
+	//index.resize(n);
+	//fill(index.begin(), index.end(), end());
+}
+
+const Vector* SparseVector::Item(int i) const{
+	//if(index[i] == end())
+    const_iterator it = find(i);
+    if(it == end())
+		return 0;
+
+	//return &(*index[i]).second;
+    return &(it->second);
+}
+
+Vector SparseVector::SubVector(int i){
+    iterator it = find(i);
+	//if(index[i] == end()){
+    if(it == end()){
+		Vector v;
+		v.Allocate(dim);
+		vec_clear(v);
+        insert(make_pair(i, v));
+		//index[i] = insert(make_pair(i, v)).first;
+		return v;
+	}
+	//return (*index[i]).second;
+    return it->second;
+}
+
+void SparseVector::Clear(int i){
+    iterator it = find(i);
+	//if(index[i] != end()){
+    if(it != end()){
+        erase(it);
+		//erase(index[i]);
+		//index[i] = end();
+	}
+}
+
+void SparseVector::Clear(){
+	clear();
+	//fill(index.begin(), index.end(), end());
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+SparseMatrix::SparseMatrix(){
+	//base = 0;
+	m   = 0;
+	n   = 0;
+	dim = 0;
+}
+
+void SparseMatrix::Resize(int _m, int _n, int _dim){
+	m   = _m;
+	n   = _n;
+	dim = _dim;
+	rows.resize(m);
+	//for(Row& r :rows){
+	//	r.index.resize(n);
+	//	fill(r.index.begin(), r.index.end(), r.end());
+	//}
+}
+
+const Matrix* SparseMatrix::Item(int i, int j) const{
+    Row::const_iterator it = rows[i].find(j);
+	//if(rows[i].index[j] == rows[i].end())
+    if(it == rows[i].end())
+		return 0;
+
+    return &(it->second);
+	//return &(*rows[i].index[j]).second;
+}
+
+Matrix SparseMatrix::SubMatrix(int i, int j){
+    Row::iterator it = rows[i].find(j);
+    if(it == rows[i].end()){
+	//if(rows[i].index[j] == rows[i].end()){
+		Matrix m;
+		m.Allocate(dim, dim);
+		mat_clear(m);
+        rows[i].insert(make_pair(j, m));
+		//rows[i].index[j] = rows[i].insert(make_pair(j, m)).first;
+		return m;
+	}
+    return it->second;
+	//return (*rows[i].index[j]).second;
+}
+
+Matrix SparseMatrix::SubMatrix(int i, int j) const{
+    return rows[i].find(j)->second;
+	//return (*rows[i].index[j]).second;
+}
+
+SparseMatrix SparseMatrix::RowMatrix(int i) const{
+	SparseMatrix y;
+	y.Resize(1, n, dim);
+	for(Row::const_iterator it = rows[i].begin(); it != rows[i].end(); it++){
+		int           j  = it->first;
+		const Matrix& _m = it->second;
+        y.rows[0].insert(make_pair(j, _m));
+		//y.rows[0].index[j] = y.rows[0].insert(make_pair(j, _m)).first;
+	}
+	return y;
+}
+
+//SparseMatrix SparseMatrix::ColMatrix(int j) const{
+//	SparseMatrix y;
+//	y.dim        = dim;
+//	y.base       = (SparseMatrix*)this;
+//	y.row_offset = 0;
+//	y.col_offset = j;
+//	
+//	for(const_iterator it = begin(); it != end(); it++){
+//		if(it->first.second == j){
+//			y.insert(make_pair(make_pair(it->first.first, 0), it->second));
+//		}
+//	}
+//	return y;
+//}
+
+void SparseMatrix::Clear(){
+	for(Row& row : rows){
+		row.clear();
+		//fill(row.index.begin(), row.index.end(), row.end());
+	}
+}
+
+void SparseMatrix::RowClear(int i){
+	rows[i].clear();
+	//fill(rows[i].index.begin(), rows[i].index.end(), rows[i].end());
+}
+
+void SparseMatrix::ColClear(int j){
+	for(int i = 0; i < m; i++){
+		for(Row::iterator it = rows[i].begin(); it != rows[i].end(); ){
+			if(it->first == j){
+				 it = rows[i].erase(it);
+				 //rows[i].index[j] = rows[i].end();
+			}
+			else it++;
+		}
+	}
+}
+
+void SparseMatrix::PrintSparsity(ostream& os){
+	for(int i = 0; i < m; i++){
+		int j = 0;
+		for(Row::iterator it = rows[i].begin(); it != rows[i].end(); it++){
+			while(j < it->first){
+				os << '0' << ' ';
+				j++;
+			}
+			os << '1' << ' ';
+			j++;
+		}
+		while(j < n){
+			os << '0' << ' ';
+			j++;
+		}
+		os << '\n';
+	}
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+void  mat_eye(Matrix& m){
+	for(int j = 0; j < m.n; j++)for(int i = 0; i < m.m; i++)
+		m(i,j) = (i == j ? 1.0f : 0.0f);
+}
+
+void  mat_diag(const Vector& v, Matrix& m){
+	m.Allocate(v.n, v.n);
+	for(int j = 0; j < m.n; j++)for(int i = 0; i < m.m; i++)
+		m(i,j) = (i == j ? v.vh[i] : 0.0f);
+}
+
+void  mat_eig(const Matrix& m, Vector& wr, Vector& wi, Matrix& vl, Matrix& vr){
+	wr.Allocate(m.m);
+	wi.Allocate(m.m);
+	vl.Allocate(m.m, m.m);
+	vr.Allocate(m.m, m.m);
+	Matrix tmp;
+	tmp.Allocate(m.m, m.m);
+	mat_copy(m, tmp);
+	LAPACKE_dgeev(LAPACK_COL_MAJOR, 'V', 'V', m.m, &tmp.vh[0], tmp.l, &wr.vh[0], &wi.vh[0], &vl.vh[0], vl.l, &vr.vh[0], vr.l);
+}
+
+// inverse of general matrix
+void mat_inv_gen(const Matrix& m, Matrix& y){
+	mat_copy(m, y);
+	vector<int> pivot; pivot.resize(m.m);
+	LAPACKE_dgetrf(LAPACK_COL_MAJOR, m.m, m.m, &y.vh[0], y.l, &pivot[0]);
+	LAPACKE_dgetri(LAPACK_COL_MAJOR, m.m,      &y.vh[0], y.l, &pivot[0]);
+}
+
+// inverse of symmetric positive definite matrix
+void mat_inv_pd(const Matrix& m, Matrix& y){
+	mat_copy(m, y);
+	LAPACKE_dpotrf(LAPACK_COL_MAJOR, 'U', m.m, y.vh, y.l);
+	LAPACKE_dpotri(LAPACK_COL_MAJOR, 'U', m.m, y.vh, y.l);
+}
+
+void symmat_solve  (const Matrix& A, const Vector& b, Vector& x){
+
+}
+
+void vec_clear(Vector& y){
+	vec_clear(std::move(y));
+}
+void vec_clear(Vector&& y){
+	double* vh = y.vh;
+	for(int i = 0; i < y.n; i++)
+		*vh++ = 0.0;
+}
+
+void vec_clear(SparseVector& y){
+	for(SparseVector::iterator it = y.begin(); it != y.end(); it++)
+		vec_clear(it->second);
+}
+
+void mat_clear(Matrix& y){
+	mat_clear(std::move(y));
+}
+void mat_clear(Matrix&& y){
+	double* vh0 = y.vh;
+	for(int j = 0; j < y.n; j++, vh0 += y.l){
+		double* vh = vh0;
+		for(int i = 0; i < y.m; i++, vh++){
+			*vh = 0.0;
+		}
+	}
+}
+
+void mat_clear(SparseMatrix& y){
+	for(int i = 0; i < y.m; i++){
+		for(SparseMatrix::Row::iterator it = y.rows[i].begin(); it != y.rows[i].end(); it++)
+			mat_clear(it->second);
+	}
+}
+
+void vec_copy(const Vector& v1, Vector& y){
+	vec_copy(v1, std::move(y));
+}
+void vec_copy(const Vector& v1, Vector&& y){
+	double* vh0 = v1.vh;
+	double* vh1 = y .vh;
+	for(int i = 0; i < v1.n; i++)
+		*vh1++ = *vh0++;
+}
+
+void vec_copy (const SparseVector& v1, SparseVector& y){
+	for(SparseVector::const_iterator it = v1.begin(); it != v1.end(); it++){
+		int           j  = it->first;
+		const Vector& vj = it->second;
+		Vector yj = y.SubVector(j);
+		vec_copy(vj, yj);
+	}
+}
+
+void mat_copy(const Matrix& m1, Matrix& y){
+	mat_copy(m1, std::move(y));
+}
+void mat_copy(const Matrix& m1, Matrix&& y){
+	double* col0 = m1.vh;
+	double* col1 = y .vh;
+	for(int j = 0; j < m1.n; j++, col0 += m1.l, col1 += y.l){
+		double* v0 = col0;
+		double* v1 = col1;
+		for(int i = 0; i < m1.m; i++){
+			*v1++ = *v0++;
+		}
+	}
+}
+
+void mat_copy (const SparseMatrix& m1, SparseMatrix& y){
+	for(int i = 0; i < m1.m; i++){
+		for(SparseMatrix::Row::const_iterator it = m1.rows[i].begin(); it != m1.rows[i].end(); it++){
+			int           j   = it->first ;
+			const Matrix& mij = it->second;
+			Matrix yij = y.SubMatrix(i, j);
+			mat_copy(mij, yij);
+		}
+	}
+}
+
+void vec_add(const Vector& v1, Vector& y){
+	vec_add(v1, std::move(y));
+}
+void vec_add(const Vector& v1, Vector&& y){
+	double* vh0 = v1.vh;
+	double* vh1 = y .vh;
+	for(int i = 0; i < v1.n; i++)
+		*vh1++ += *vh0++;
+}
+
+void vec_add(const SparseVector& v1, SparseVector& y){
+	for(SparseVector::const_iterator it = v1.begin(); it != v1.end(); it++){
+		int           j  = it->first;
+		const Vector& vj = it->second;
+		Vector yj = y.SubVector(j);
+		vec_add(vj, yj);
+	}
+}
+
+void mat_add(const Matrix& m1, Matrix& y){
+	mat_add(m1, std::move(y));
+}
+void mat_add(const Matrix& m1, Matrix&& y){
+	double* col0 = m1.vh;
+	double* col1 = y .vh;
+	for(int j = 0; j < m1.n; j++, col0 += m1.l, col1 += y.l){
+		double* v0 = col0;
+		double* v1 = col1;
+		for(int i = 0; i < m1.m; i++){
+			*v1++ += *v0++;
+		}
+	}
+}
+
+void mat_add(const SparseMatrix& m1, SparseMatrix& y){
+	for(int i = 0; i < m1.m; i++){
+		for(SparseMatrix::Row::const_iterator it = m1.rows[i].begin(); it != m1.rows[i].end(); it++){
+			int           j   = it->first ;
+			const Matrix& mij = it->second;
+			Matrix yij = y.SubMatrix(i, j);
+			mat_add(mij, yij);
+		}
+	}
+}
+
+double mat_abs(const Matrix& m){
+	// assumes m.m == m.l
+	return cblas_dasum(m.m*m.n, m.vh, 1);
+}
+
+double vec_dot(const Vector& v1, const Vector& v2){
+	return cblas_ddot(v1.n, v1.vh, 1, v2.vh, 1);
+}
+
+void mat_vec_mul(const Matrix& m1, const Vector& v, Vector& y, double alpha, double beta){
+	mat_vec_mul(m1, v, std::move(y), alpha, beta);
+}
+
+void mat_vec_mul(const Matrix& m1, const Vector& v, Vector&& y, double alpha, double beta){
+	Vector tmp;
+	if(v.vh == y.vh){
+		if(tmp.n != y.n)
+			tmp.Allocate(y.n);
+		vec_copy(y, tmp);
+		mat_vec_mul(m1, v, tmp, alpha, beta);
+		vec_copy(tmp, y);
+	}
+	else{
+		cblas_dgemv(CblasColMajor, CblasNoTrans, m1.m, m1.n, alpha, m1.vh, m1.l, v.vh, 1, beta, y.vh, 1);
+	}
+}
+
+void mat_vec_mul(const SparseMatrix& m1, const SparseVector& v, Vector& y, double alpha, double beta){
+	for(SparseMatrix::Row::const_iterator it = m1.rows[0].begin(); it != m1.rows[0].end(); it++){
+		int           j   = it->first ;
+		const Matrix& m1j = it->second;
+		const Vector* vj  = v.Item(j);
+		if(vj)
+			mat_vec_mul(m1j, *vj, y, alpha, (it == m1.rows[0].begin() ? beta : 1.0));
+	}
+}
+
+void mattr_vec_mul(const Matrix& m1, const Vector& v, Vector& y, double alpha, double beta){
+	mattr_vec_mul(m1, v, std::move(y), alpha, beta);
+}
+void mattr_vec_mul(const Matrix& m1, const Vector& v, Vector&& y, double alpha, double beta){
+	Vector tmp;
+	if(v.vh == y.vh){
+		if(tmp.n != y.n)
+			tmp.Allocate(y.n);
+		vec_copy(y, tmp);
+		mattr_vec_mul(m1, v, tmp, alpha, beta);
+		vec_copy(tmp, y);
+	}
+	else{
+		cblas_dgemv(CblasColMajor, CblasTrans, m1.m, m1.n, alpha, m1.vh, m1.l, v.vh, 1, beta, y.vh, 1);
+	}
+}
+
+void mattr_vec_mul(const SparseMatrix& m1, const Vector& v, SparseVector& y, double alpha, double beta){
+	for(SparseMatrix::Row::const_iterator it = m1.rows[0].begin(); it != m1.rows[0].end(); it++){
+		int           j  = it->first ;
+		const Matrix& mj = it->second;
+		Vector yj = y.SubVector(j);
+		mattr_vec_mul(mj, v, yj, alpha, beta);
+	}
+}
+
+void symmat_vec_mul(const Matrix& m1, const Vector& v, Vector& y, double alpha, double beta){
+	cblas_dsymv(CblasColMajor, CblasUpper, m1.m, alpha, m1.vh, m1.l, v.vh, 1, beta, y.vh, 1);
+}
+
+void mat_mat_mul(const Matrix& m1, const Matrix& m2, Matrix& y, double alpha, double beta){
+	mat_mat_mul(m1, m2, std::move(y), alpha, beta);
+}
+void mat_mat_mul(const Matrix& m1, const Matrix& m2, Matrix&& y, double alpha, double beta){
+	Matrix tmp;
+	if(m1.vh == y.vh || m2.vh == y.vh){
+		if(tmp.m != y.m || tmp.n != y.n)
+			tmp.Allocate(y.m, y.n);
+		mat_copy(y, tmp);
+		mat_mat_mul(m1, m2, tmp, alpha, beta);
+		mat_copy(tmp, y);
+	}
+	else{
+		cblas_dgemm(CblasColMajor, CblasNoTrans, CblasNoTrans, m1.m, m2.n, m2.m, alpha, m1.vh, m1.l, m2.vh, m2.l, beta, y.vh, y.l);
+	}
+}
+
+//void mat_mat_mul(const SparseMatrix& m1, const Matrix& m2, SparseMatrix& y, double alpha, double beta){
+//	for(SparseMatrix::Row::const_iterator it = m1.begin(); it != m1.end(); it++){
+//		int           i  = it->first.first;
+//		const Matrix& mi = it->second;
+//		mat_mat_mul(mi, m2, y.SubMatrix(i, 0), alpha, beta);
+//	}
+//}
+
+void mattr_mat_mul(const Matrix& m1, const Matrix& m2, Matrix& y, double alpha, double beta){
+	mattr_mat_mul(m1, m2, std::move(y), alpha, beta);
+}
+void mattr_mat_mul(const Matrix& m1, const Matrix& m2, Matrix&& y, double alpha, double beta){
+	Matrix tmp;
+	if(m1.vh == y.vh || m2.vh == y.vh){
+		if(tmp.m != y.m || tmp.n != y.n)
+			tmp.Allocate(y.m, y.n);
+		mat_copy(y, tmp);
+		mattr_mat_mul(m1, m2, tmp, alpha, beta);
+		mat_copy(tmp, y);
+	}
+	else{
+		cblas_dgemm(CblasColMajor, CblasTrans, CblasNoTrans, m1.n, m2.n, m1.m, alpha, m1.vh, m1.l, m2.vh, m2.l, beta, y.vh, y.l);
+	}
+}
+
+void mattr_mat_mul (const Matrix& m1, const SparseMatrix& m2, SparseMatrix& y, double alpha, double beta){
+	for(SparseMatrix::Row::const_iterator it = m2.rows[0].begin(); it != m2.rows[0].end(); it++){
+		int           j  = it->first ;
+		const Matrix& mj = it->second;
+		mattr_mat_mul(m1, mj, y.SubMatrix(0, j), alpha, beta);
+	}
+}
+
+void mattr_mat_mul (const SparseMatrix& m1, const SparseMatrix& m2, SparseMatrix& y, double alpha, double beta){
+	for(SparseMatrix::Row::const_iterator it1 = m1.rows[0].begin(); it1 != m1.rows[0].end(); it1++)
+	for(SparseMatrix::Row::const_iterator it2 = m2.rows[0].begin(); it2 != m2.rows[0].end(); it2++){
+		int           i  = it1->first ;
+		int           j  = it2->first ;
+		const Matrix& mi = it1->second;
+		const Matrix& mj = it2->second;
+		mattr_mat_mul(mi, mj, y.SubMatrix(i, j), alpha, beta);
+	}
+}
+
+void mat_mattr_mul(const Matrix& m1, const Matrix& m2, Matrix& y, double alpha, double beta){
+	cblas_dgemm(CblasColMajor, CblasNoTrans, CblasTrans, m1.m, m2.m, m1.n, alpha, m1.vh, m1.l, m2.vh, m2.l, beta, y.vh, y.l);
+}
+
+void symmat_mat_mul(const Matrix& m1, const Matrix& m2, Matrix& y, double alpha, double beta){
+	symmat_mat_mul(m1, m2, std::move(y), alpha, beta);
+}
+void symmat_mat_mul(const Matrix& m1, const Matrix& m2, Matrix&& y, double alpha, double beta){
+	cblas_dsymm(CblasColMajor, CblasLeft, CblasUpper, m1.m, m2.n, alpha, m1.vh, m1.l, m2.vh, m2.l , beta, y.vh, y.l);
+}
+
+void symmat_mat_mul(const Matrix& m1, const SparseMatrix& m2, SparseMatrix& y, double alpha, double beta){
+	for(SparseMatrix::Row::const_iterator it = m2.rows[0].begin(); it != m2.rows[0].end(); it++){
+		int           j  = it->first ;
+		const Matrix& mj = it->second;
+		symmat_mat_mul(m1, mj, y.SubMatrix(0, j), alpha, beta);
+	}
+}
+
+void Linsolve(SparseMatrix& A, SparseVector& b, SparseVector& x, bool mindeg, real_t regularization){
+	vector<int> order;
+	set<int> queue;
+	vector<Matrix>        Aii;
+	vector<Matrix>        Aii_inv;
+	vector<SparseMatrix>  Arow;
+	vector<Vector>        bi;
+	vector<Vector>        Aii_inv_bi;
+	vector<SparseMatrix>  Aii_inv_Arow;
+
+	Aii         .resize(A.m);
+	Aii_inv     .resize(A.m);
+	Arow        .resize(A.m);
+	bi          .resize(A.m);
+	Aii_inv_bi  .resize(A.m);
+	Aii_inv_Arow.resize(A.m);
+
+	for(int i = 0; i < A.m; i++){
+		Aii         [i].Allocate(A.dim, A.dim);
+		Aii_inv     [i].Allocate(A.dim, A.dim);
+		Arow        [i].Resize(1, A.n, A.dim);
+		bi          [i].Allocate(A.dim);
+		Aii_inv_bi  [i].Allocate(A.dim);
+		Aii_inv_Arow[i].Resize(1, A.n, A.dim);
+
+		queue.insert(i);
+	}
+
+	while(!queue.empty()){
+		// index with minimum degree
+		//int i = A.MinimumDegreeIndex();
+		set<int>::iterator it;
+		if(mindeg){
+			it = std::min_element(queue.begin(), queue.end(), [&A](int i0, int i1){ return A.rows[i0].size() < A.rows[i1].size(); });
+		}
+		else{
+			it = queue.begin();
+		}
+		int i = *it;
+		order.push_back(i);
+		queue.erase(it);
+
+		mat_copy(A.RowMatrix(i), Arow[i]);
+		mat_copy(Arow[i].SubMatrix(0, i), Aii[i]);
+		vec_copy(b.SubVector(i), bi[i]);
+
+		Arow[i].ColClear(i);
+		A.ColClear(i);
+		A.RowClear(i);
+		b.Clear(i);
+
+		for(int j = 0; j < A.dim; j++)
+			Aii[i](j,j) += regularization;
+		mat_inv_pd(Aii[i], Aii_inv[i]);
+
+		symmat_vec_mul(Aii_inv[i], bi[i], Aii_inv_bi[i], 1.0, 0.0);
+
+		mattr_vec_mul(Arow[i], Aii_inv_bi[i], b, -1.0, 1.0);
+
+		symmat_mat_mul(Aii_inv[i], Arow[i]        , Aii_inv_Arow[i],  1.0, 0.0);
+		mattr_mat_mul (Arow[i]   , Aii_inv_Arow[i], A              , -1.0, 1.0);
+	}
+
+	for(vector<int>::reverse_iterator it = order.rbegin(); it != order.rend(); it++){
+		int i = *it;
+		mat_vec_mul   (Arow[i]   , x    , bi[i]         ,  1.0, 1.0);
+		
+		Vector xi = x.SubVector(i);
+		symmat_vec_mul(Aii_inv[i], bi[i], xi, -1.0, 0.0);
+	}
+}
+
+}
