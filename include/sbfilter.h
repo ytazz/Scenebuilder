a8964cdb8fad29dab77ae3b3f49b8776b7aa34ad
Yuichi Tazaki <tazaki@mech.kobe-u.ac.jp>
2024-02-21 13:33:54 +0900

    ;

--- include/sbfilter.h
+++ include/sbfilter.h
@@ -7,36 +7,30 @@ namespace Scenebuilder{;
 /*
  1st order linear filter
  */
-struct FilterLinear{
-    real_t w;
-    real_t u;
-    real_t yd, y;
-    bool   first;
-    real_t yd_max;
+class Filter{
+public:
+    struct Type{
+        enum{
+            None,
+            FirstOrderLPF,
+            FirstOrderLPF2,
+            Butterworth3,
+        };
+    };
 
+    int     type;
+    real_t  w, w2, w3;
+    real_t  u;
+    real_t  ydd, yd, y;
+    bool    first;
+    real_t  yd_max;
+
+public:
     void SetCutoff(real_t f);
 
     real_t operator()(real_t _u, real_t dt);
 
-    FilterLinear();
-};
-
-/*
- 3rd order butterworth filter
- */
-struct FilterButterworth3{
-    real_t w, w2, w3;
-    real_t u;
-    real_t ydd, yd, y;
-    bool   first;
-    real_t yd_max;
-
-    // set cutoff frequency [Hz]
-    void   SetCutoff(real_t f);
-
-    real_t operator()(real_t _u, real_t dt);
-
-    FilterButterworth3();
+    Filter();
 };
 
 }
--- src/sbfilter.cpp
+++ src/sbfilter.cpp
@@ -6,43 +6,7 @@ const real_t pi = 3.14159265368979;
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 
-FilterLinear::FilterLinear(){
-    u      = 0.0;
-    yd     = 0.0;
-    y      = 0.0;
-    w      = 1.0;
-    yd_max = 0.0;
-
-    first = true;
-}
-
-void FilterLinear::SetCutoff(real_t f){
-    w   = 2.0*pi*f;
-}
-
-real_t FilterLinear::operator()(real_t _u, real_t dt){
-    if(first){
-        y = _u;
-        first = false;
-    }
-
-    u    = _u;
-    yd   = -w*(y - u);
-
-    if(yd_max > 0.0){
-        yd = std::min(std::max(-yd_max, yd), yd_max);
-    }
-
-    y += yd*dt;
-    //real_t a = exp(-w*dt);
-    //y    = a*y + (1.0 - a)*u;
-   
-    return y;
-}
-
-///////////////////////////////////////////////////////////////////////////////////////////////////
-
-FilterButterworth3::FilterButterworth3(){
+Filter::Filter(){
     u      = 0.0;
     ydd    = 0.0;
     yd     = 0.0;
@@ -52,31 +16,51 @@ FilterButterworth3::FilterButterworth3(){
     w3     = w*w2;
     yd_max = 0.0;
 
+    type  = Type::FirstOrderLPF;
     first = true;
 }
 
-void FilterButterworth3::SetCutoff(real_t f){
+void Filter::SetCutoff(real_t f){
     w   = 2.0*pi*f;
     w2  = w*w;
     w3  = w*w2;
 }
 
-real_t FilterButterworth3::operator()(real_t _u, real_t dt){
+real_t Filter::operator()(real_t _u, real_t dt){
     if(first){
         y = _u;
         first = false;
     }
 
-    u    = _u;
-    y   += yd*dt;
-    yd  += ydd*dt;
+    u = _u;
 
-    if(yd_max > 0.0){
-        yd = std::min(std::max(-yd_max, yd), yd_max);
+    if(type == Type::None){
+        y = u;
     }
+    if(type == Type::FirstOrderLPF){
+        yd   = -w*(y - u);
 
-    ydd += (-2.0*w*ydd - 2.0*w2*yd - w3*y + w3*u)*dt;
+        if(yd_max > 0.0){
+            yd = std::min(std::max(-yd_max, yd), yd_max);
+        }
 
+        y += yd*dt;
+    }
+    if(type == Type::FirstOrderLPF2){
+        real_t a = exp(-w*dt);
+        y    = a*y + (1.0 - a)*u;
+    }
+    if(type == Type::Butterworth3){
+        y   += yd*dt;
+        yd  += ydd*dt;
+
+        if(yd_max > 0.0){
+            yd = std::min(std::max(-yd_max, yd), yd_max);
+        }
+
+        ydd += (-2.0*w*ydd - 2.0*w2*yd - w3*y + w3*u)*dt;
+    }
+   
     return y;
 }
 
